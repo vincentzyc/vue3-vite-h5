639500ed751ca65b01b2a76e45b8deb43c9a261f
vincentzyc <vincentzyc@qq.com>
2023-02-17 09:21:34 +0800

    🎉 init: init

---
+++ src/test/HelloWorld.test.ts
@@ -0,0 +1,29 @@
+import { mount } from '@vue/test-utils'
+import { Lazyload } from "vant"
+import HelloWorld from '@/components/HelloWorld.vue'
+
+test('mount component', async () => {
+  expect(HelloWorld).toBeTruthy()
+
+  const wrapper = mount(HelloWorld, {
+    props: {
+      count: 4,
+    },
+    global: {
+      directives: {
+        lazy: Lazyload as any
+      }
+    },
+  })
+
+  expect(wrapper.text()).toContain('4 x 2 = 8')
+  expect(wrapper.html()).toMatchSnapshot()
+
+  await wrapper.get('button').trigger('click')
+
+  expect(wrapper.text()).toContain('4 x 3 = 12')
+
+  await wrapper.get('button').trigger('click')
+
+  expect(wrapper.text()).toContain('4 x 4 = 16')
+})
