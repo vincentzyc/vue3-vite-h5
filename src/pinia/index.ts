a408b6df8c5d7a1d2208895088db7551b3a45b71
vincentzyc <vincentzyc@qq.com>
2023-02-21 09:23:19 +0800

    🎉 init: init

---
+++ src/pinia/index.ts
@@ -0,0 +1,28 @@
+import { PageIdLocation } from '@/api/types/common';
+import { defineStore } from 'pinia'
+
+export const useMainStore = defineStore('main', {
+  state: () => ({
+    counter: 0,
+    pid: '',
+    cjData: null as PageIdLocation | null,
+  }),
+  getters: {
+    doubleCount: (state) => state.counter * 2,
+    doubleCountPlusOne(): number {
+      return this.doubleCount * 2 + 1
+    },
+    locationCity: state => [state.cjData?.province || '', state.cjData?.city || '']
+  },
+  actions: {
+    setPid(payload: string) {
+      this.pid = payload;
+    },
+    setCjData(payload: PageIdLocation | null) {
+      this.cjData = payload;
+    },
+    reset() {
+      this.counter = 0
+    },
+  },
+})
