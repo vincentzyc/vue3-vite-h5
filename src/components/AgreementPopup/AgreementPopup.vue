0d556abc8d61d30d8568f6b446748598f516930c
vincentzyc <vincentzyc@qq.com>
2023-02-23 10:46:50 +0800

    🎉 init: init

---
+++ src/components/AgreementPopup/AgreementPopup.vue
@@ -0,0 +1,28 @@
+<template>
+  <van-popup v-model:show="isShow" class="agreement-popup" style="max-width: 576px" teleport="body" @closed="close">
+    <div class="agreement-wrap">
+      <iframe :src="text" frameborder="0" style="width: 100%; height: 100%"></iframe>
+    </div>
+    <div class="agreement-confirm" @click="close()">我知道了</div>
+  </van-popup>
+</template>
+
+<script setup lang="ts">
+import "./style.less";
+const props = defineProps<{
+  show: boolean;
+  text: string;
+}>();
+const emits = defineEmits<{
+  (e: "update:show", bool: boolean): void;
+}>();
+
+let isShow = computed({
+  get: () => props.show,
+  set: v => emits("update:show", v),
+});
+
+function close() {
+  isShow.value = false;
+}
+</script>
