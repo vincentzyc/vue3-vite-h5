b17729e7e9ca744641a5eac00d129224824bdd0b
vincentzyc <vincentzyc@qq.com>
2023-02-24 09:13:28 +0800

    🎉 init: init

---
+++ src/components/FormItem/FormName.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex align-middle form-item">
+    <div class="form-label">领卡姓名</div>
+    <div class="form-input flex flex-auto align-middle">
+      <input
+        @blur="checkName(model)"
+        :maxlength="15"
+        placeholder="输入办理人姓名（已加密）"
+        type="text"
+        v-model.trim="model"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { checkName } from '@/composition/business/useVerifyData'
+
+export default defineComponent({
+  name: "FormName",
+  props: {
+    modelValue: {
+      required: true,
+      type: String,
+    }
+  },
+  emits: ['update:modelValue'],
+  setup(props, { emit }) {
+    const model = computed<string>({
+      get() {
+        return props.modelValue;
+      },
+      set(val) {
+        emit("update:modelValue", val);
+      }
+    })
+    return {
+      model,
+      checkName,
+    }
+  }
+})
+</script>
