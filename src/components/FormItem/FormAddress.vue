2f57411e46415416b956242e5ba75b52a80f28fd
vincentzyc <vincentzyc@qq.com>
2023-02-25 11:40:11 +0800

    🎉 init: init

---
+++ src/components/FormItem/FormAddress.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="flex align-middle form-item border-bnone">
+    <div class="form-label">详细地址</div>
+    <div class="form-input flex-auto flex align-middle">
+      <input
+        @blur="checkAddress(model)"
+        :maxlength="50"
+        placeholder="街道/镇+村/小区/楼+门牌号"
+        type="text"
+        v-model.trim="model"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+import { checkAddress } from '@/composition/business/useVerifyData'
+
+
+export default defineComponent({
+  name: "FormAddress",
+  props: {
+    modelValue: {
+      required: true,
+      type: String,
+    }
+  },
+  emits: ['update:modelValue'],
+  setup(props, { emit }) {
+    const model = computed<string>({
+      get() {
+        return props.modelValue;
+      },
+      set(val) {
+        emit("update:modelValue", val);
+      }
+    })
+    return {
+      model,
+      checkAddress,
+    }
+  }
+})
+</script>
