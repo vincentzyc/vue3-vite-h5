b3769a7d4f4a8a4bd06853b8a1ea1b83a46ea5d9
vincentzyc <vincentzyc@qq.com>
2023-02-25 11:41:13 +0800

    🎉 init: init

---
+++ src/components/FormItem/FormIDCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex align-middle form-item">
+    <div class="form-label">身份证号</div>
+    <div class="form-input flex-auto flex align-middle">
+      <input
+        @blur="checkIDCard(model)"
+        :maxlength="18"
+        placeholder="输入办理人身份证（已加密）"
+        type="text"
+        v-model.trim="model"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { checkIDCard } from '@/composition/business/useVerifyData'
+
+export default defineComponent({
+  name: "FormIDCard",
+  props: {
+    modelValue: {
+      required: true,
+      type: String,
+    }
+  },
+  emits: ['update:modelValue'],
+  setup(props, { emit }) {
+    const model = computed<string>({
+      get() {
+        return props.modelValue;
+      },
+      set(val) {
+        emit("update:modelValue", val);
+      }
+    })
+    return {
+      model,
+      checkIDCard,
+    }
+  }
+})
+</script>
