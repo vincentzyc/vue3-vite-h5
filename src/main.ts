d23a1e1e43ab917dd668a48048a77c85f9cdb963
vincentzyc <vincentzyc@qq.com>
2023-03-11 01:19:32 +0800

    🎉 init: init

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Axios from './plugins/axios'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import App from './App.vue';
+import Axios from './plugins/axios';
+import { createPinia } from 'pinia';
 import { Lazyload } from 'vant';
-import '@vant/touch-emulator' //vant 桌面端适配
+import '@vant/touch-emulator'; //vant 桌面端适配
 import 'vant/es/toast/style';
 import 'vant/es/dialog/style';
-import '@/assets/css/base.css'
-import '@/assets/css/main.less'
+import '@/assets/css/base.css';
+import '@/assets/css/main.less';
 
 const app = createApp(App);
 
-app.use(createPinia())
+app.use(createPinia());
 
-app.config.globalProperties.$axios = Axios
+app.config.globalProperties.$axios = Axios;
 
 app.use(Lazyload);
-app.mount('#app')
+app.mount('#app');
