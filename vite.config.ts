66d013d1a81ca7c8e5638c466422dcd8165ffdee
vincentzyc <vincentzyc@qq.com>
2023-03-11 01:19:53 +0800

    🎉 init: init

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,43 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite'
-import { VantResolver } from 'unplugin-vue-components/resolvers'
-import AutoImport from 'unplugin-auto-import/vite'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   server: {
     host: true,
-    port: 3100
+    port: 3100,
   },
   plugins: [
     vue(),
     AutoImport({
       include: [
         /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-        /\.vue$/, /\.vue\?vue/, // .vue
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
         /\.md$/, // .md
       ],
       imports: ['vue'],
     }),
     Components({
       resolvers: [VantResolver()],
-    })
+    }),
   ],
   test: {
     globals: true,
     environment: 'jsdom',
     deps: {
-      inline: ['vant']
+      inline: ['vant'],
     },
   },
   resolve: {
     alias: {
-      "@": "/src"
-    }
-  }
-})
+      '@': '/src',
+    },
+  },
+});
