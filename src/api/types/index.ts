466df15121aa1543dc3b399f0da5153757506e58
vincentzyc <vincentzyc@qq.com>
2023-03-13 09:35:54 +0800

    🎉 init: init

--- src/api/types/index.ts
+++ src/api/types/index.ts
@@ -1,8 +1,8 @@
 export interface ApiModule {
-  name: string,
-  url: string,
-  data?: any,
-  type?: string,
-  getError?: boolean,
-  getAllData?: boolean
+  name: string;
+  url: string;
+  data?: any;
+  type?: string;
+  getError?: boolean;
+  getAllData?: boolean;
 }
