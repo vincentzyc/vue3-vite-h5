8498a16dd9143a12ca6d09e9b8866d0c8b3002ae
vincentzyc <vincentzyc@qq.com>
2023-03-16 09:35:13 +0800

    🎉 init: init

--- src/utils/__tests__/validate/link.spec.ts
+++ src/utils/__tests__/validate/link.spec.ts
@@ -1,13 +1,59 @@
 import { isLink } from '../../validate/link';
 
-test('isEmail', () => {
-  expect(isLink('weixin://xxx')).toBeTruthy();
-  expect(isLink('abc@gmail.com')).toBeFalsy();
-  expect(isLink('192.168.1.1')).toBeTruthy();
-  expect(isLink('https://www.baidu.com')).toBeTruthy();
-  expect(isLink('//www.baidu.com')).toBeFalsy();
-  expect(isLink('www.baidu.com')).toBeTruthy();
-  expect(isLink('http:xxx.abc')).toBeFalsy();
-  expect(isLink('http://xxx.abc')).toBeTruthy();
-  expect(isLink('ftp://xxx.abc')).toBeTruthy();
+// test('isEmail', () => {
+//   expect(isLink('weixin://xxx')).toBeTruthy();
+//   expect(isLink('abc@gmail.com')).toBeFalsy();
+//   expect(isLink('192.168.1.1')).toBeTruthy();
+//   expect(isLink('https://www.baidu.com')).toBeTruthy();
+//   expect(isLink('//www.baidu.com')).toBeFalsy();
+//   expect(isLink('www.baidu.com')).toBeTruthy();
+//   expect(isLink('http:xxx.abc')).toBeFalsy();
+//   expect(isLink('http://xxx.abc')).toBeTruthy();
+//   expect(isLink('ftp://xxx.abc')).toBeTruthy();
+// });
+
+// 下面是使用chatCPT编写的单元测试
+test('isLink returns true for weixin links', () => {
+  const link = 'weixin://example';
+  expect(isLink(link)).toBe(true);
+});
+
+test('isLink returns true for valid links', () => {
+  const link = 'https://www.example.com';
+  expect(isLink(link)).toBe(true);
+});
+
+test('isLink returns false for invalid links', () => {
+  const link = 'invalid link';
+  expect(isLink(link)).toBe(false);
+});
+
+test('isLink returns false for empty string', () => {
+  const link = '';
+  expect(isLink(link)).toBe(false);
+});
+
+test('isLink returns false for invalid URLs', () => {
+  const link = 'http://.com';
+  expect(isLink(link)).toBe(false);
+});
+
+test('isLink returns true for URLs with query parameters', () => {
+  const link = 'https://www.example.com/?param=value';
+  expect(isLink(link)).toBe(true);
+});
+
+test('isLink returns true for URLs with hash', () => {
+  const link = 'https://www.example.com/#hash';
+  expect(isLink(link)).toBe(true);
+});
+
+test('isLink returns false for phone numbers', () => {
+  const link = 'tel:1234567890';
+  expect(isLink(link)).toBe(false);
+});
+
+test('isLink returns false for email addresses', () => {
+  const link = 'mailto:john@example.com';
+  expect(isLink(link)).toBe(false);
 });
--- src/utils/__tests__/validate/number.spec.ts
+++ src/utils/__tests__/validate/number.spec.ts
@@ -1,14 +1,38 @@
 import { isNumeric, isNaN } from '../../validate/number';
 
-test('isNumeric', () => {
-  expect(isNumeric('1')).toBeTruthy();
-  expect(isNumeric('1.2')).toBeTruthy();
-  expect(isNumeric('1..2')).toBeFalsy();
-  expect(isNumeric('abc')).toBeFalsy();
-  expect(isNumeric('1b2')).toBeFalsy();
+// test('isNumeric', () => {
+//   expect(isNumeric('1')).toBeTruthy();
+//   expect(isNumeric('1.2')).toBeTruthy();
+//   expect(isNumeric('1..2')).toBeFalsy();
+//   expect(isNumeric('abc')).toBeFalsy();
+//   expect(isNumeric('1b2')).toBeFalsy();
+// });
+
+// test('isNaN', () => {
+//   expect(isNaN(1)).toBeFalsy();
+//   expect(isNaN(Number('a'))).toBeTruthy();
+// });
+
+// 下面是使用chatCPT编写的单元测试
+test('isNumeric function', () => {
+  expect(isNumeric('123')).toBe(true);
+  expect(isNumeric('123.45')).toBe(true);
+  expect(isNumeric('0.123')).toBe(true);
+
+  expect(isNumeric('.123')).toBe(false);
+  expect(isNumeric('abc')).toBe(false);
+  expect(isNumeric('123a')).toBe(false);
+  expect(isNumeric('12.3.4')).toBe(false);
+  expect(isNumeric('123.')).toBe(false);
+  expect(isNumeric('.')).toBe(false);
 });
 
-test('isNaN', () => {
-  expect(isNaN(1)).toBeFalsy();
-  expect(isNaN(Number('a'))).toBeTruthy();
+test('isNaN returns true for NaN', () => {
+  expect(isNaN(NaN)).toBe(true);
+});
+
+test('isNaN returns false for numbers', () => {
+  expect(isNaN(0)).toBe(false);
+  expect(isNaN(42)).toBe(false);
+  expect(isNaN(-1.5)).toBe(false);
 });
