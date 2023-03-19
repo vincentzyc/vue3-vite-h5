9f53f03b558ee01b198c1c624ed4744a8b47eb22
vincentzyc <vincentzyc@qq.com>
2023-03-19 21:01:00 +0800

    🎉init: init

--- src/components/CountDown/__test__/CountDown.test.ts
+++ src/components/CountDown/__test__/CountDown.test.ts
@@ -1,11 +1,11 @@
-import { mount } from '@vue/test-utils'
-import CountDown from '../CountDown.vue'
+import { mount } from '@vue/test-utils';
+import CountDown from '../CountDown.vue';
 
 test('mount CountDown component', async () => {
-  expect(CountDown).toBeTruthy()
+  expect(CountDown).toBeTruthy();
 
-  const wrapper = mount(CountDown)
+  const wrapper = mount(CountDown);
 
-  expect(wrapper.find('.countdown-timeblock').exists()).toBeTruthy()
-  expect(wrapper.find('.countdown-colon').exists()).toBeTruthy()
-})
+  expect(wrapper.find('.countdown-timeblock').exists()).toBeTruthy();
+  expect(wrapper.find('.countdown-colon').exists()).toBeTruthy();
+});
