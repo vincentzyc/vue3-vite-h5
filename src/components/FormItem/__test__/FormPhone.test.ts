0aae26c70ac2b32089a9e33696fddfb6779e77d1
vincentzyc <vincentzyc@qq.com>
2023-03-22 09:25:05 +0800

    🎉init: init

--- src/components/FormItem/__test__/FormPhone.test.ts
+++ src/components/FormItem/__test__/FormPhone.test.ts
@@ -1,23 +1,54 @@
-import { mount } from '@vue/test-utils'
-import FormPhone from '../FormPhone.vue'
+import { mount } from '@vue/test-utils';
+import FormPhone from '../FormPhone.vue';
 
 test('mount FormPhone component', async () => {
-  expect(FormPhone).toBeTruthy()
+  expect(FormPhone).toBeTruthy();
 
   const wrapper = mount(FormPhone, {
     props: {
       modelValue: '',
-      'onUpdate:modelValue': (e: string) => wrapper.setProps({ modelValue: e })
-    }
-  })
+      'onUpdate:modelValue': (e: string) => wrapper.setProps({ modelValue: e }),
+    },
+  });
 
-  expect(wrapper.html()).toMatchSnapshot()
+  expect(wrapper.html()).toMatchSnapshot();
 
-  await wrapper.get('input').setValue('18812345678')
+  await wrapper.get('input').setValue('18812345678');
 
-  expect(wrapper.props('modelValue')).toBe('18812345678')
+  expect(wrapper.props('modelValue')).toBe('18812345678');
 
-  const emitEvent = wrapper.emitted('update:modelValue')
+  const emitEvent = wrapper.emitted('update:modelValue');
 
-  expect(emitEvent).toHaveLength(1)
-})
+  expect(emitEvent).toHaveLength(1);
+});
+
+// 下面是chatGPT给的单测代码
+describe('FormPhone.vue', () => {
+  it('renders input element', () => {
+    const wrapper = mount(FormPhone);
+    expect(wrapper.find('input').exists()).toBe(true);
+  });
+
+  it('emits update:modelValue event when input value changes', async () => {
+    const wrapper = mount(FormPhone);
+    const input = wrapper.find('input');
+    await input.setValue('12345678901');
+    expect(wrapper.emitted('update:modelValue')).toBeTruthy();
+    expect(wrapper.emitted('update:modelValue')![0]).toEqual(['12345678901']);
+  });
+
+  it('sets input value when modelValue prop changes', async () => {
+    const wrapper = mount(FormPhone, { props: { modelValue: '12345678901' } });
+    const input = wrapper.find('input');
+    expect(input.element.value).toBe('12345678901');
+    await wrapper.setProps({ modelValue: '9876543210' });
+    expect(input.element.value).toBe('9876543210');
+  });
+
+  it('limits input value to 11 characters', async () => {
+    const wrapper = mount(FormPhone);
+    const input = wrapper.find('input');
+    await input.setValue('12345678901');
+    expect(input.element.value).toHaveLength(11);
+  });
+});
